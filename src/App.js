cc0f61a47fd37083cabd9a2000555dfc153deeb8
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-09-04 11:59:30 +0300

    Fixed styles, added routeer, selectedPhoto attribute

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import './css/App.scss'
 import './css/normalize.css'
 import './css/skeleton.css'
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 
 function albumsWithPhotos(albums, photos) {
   var result = [];
@@ -21,23 +23,30 @@ function photosForAlbum(albumId, photos) {
   return photos.filter(photo => albumId === photo.albumId);
 }
 
-console.log(photosForAlbum);
-
 function Album(props) {
   return (
-    <div className="album" onClick={() => props.focusOnAlbum(props.album.id)}>
-      <img alt="album cover" className='photo' src={props.album.coverPhoto}/>
-      <p className='title'>{props.album.title}</p>
-      <p className='author'>{props.album.author}</p>
-      <p className='album-length'>{props.album.photosLength}</p>
-    </div>
+    <Link to={`/albums/${props.album.id}`}>
+      <div className='album'>
+        <img alt='album cover' className='photo' src={props.album.coverPhoto}/>
+        <p className='title'>{props.album.title}</p>
+        <p className='author'>{props.album.author}</p>
+        <p className='album-length'>{props.album.photosLength}</p>
+      </div>
+    </Link>
+  )
+}
+
+function Photo(props) {
+  return (
+    <img alt='photo' src={props.photo.thumbnailUrl} onClick={() => props.selectPhoto(props.photo)} />
   )
 }
 
 function App() {
   const [albums, setAlbums] = useState([]);
   const [photos, setPhotos] = useState([]);
-  const [albumInFocus, setAlbumInFocus] = useState(null);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users/1/albums')
@@ -49,17 +58,34 @@ function App() {
     .then(json => setPhotos(json));
   }, []);
 
+  console.log(selectedPhoto);
+
   return (
-    <div className="container">
-      <div className="albums">
-        {albumInFocus
-          ? photosForAlbum(albumInFocus, photos)
-          : albumsWithPhotos(albums, photos).map((album, i) => {
-              return <Album key={"album-" + album.id} album={album} focusOnAlbum={setAlbumInFocus} />
-            })
-        }
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/albums/:albumId'>
+          <div className='photo-list'>
+            {photosForAlbum(1, photos).map((photo, i) => {
+              return <Photo key={'photo-' + photo.thumbnailUrl} photo={photo} selectPhoto={setSelectedPhoto} />
+            })}
+            {selectedPhoto
+              ? <div className='background'> <img className='selected-photo' src={selectedPhoto.url}/></div>
+              : <div></div>
+            }
+          </div>
+        </Route>
+        <Route path='/'>
+          <div className='container'>
+            <h2 className='header'>Select an album</h2>
+            <div className='albums'>
+              {albumsWithPhotos(albums, photos).map((album, i) => {
+                return <Album key={'album-' + album.id} album={album} />
+              })}
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
